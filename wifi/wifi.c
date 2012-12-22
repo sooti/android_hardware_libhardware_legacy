017c768fd8e4df5133c106d172c07aa6cd60ad79
codeworkx <codeworkx@cyanogenmod.org>
2012-12-22 13:04:14 +0100

    wifi: fix samsung wifi logic
    Change-Id: Ib065c0dffa1265a17f5e4fd6ec0b0ad36bbc7b14
    
    Conflicts:
    	wifi/wifi.c

--- wifi/wifi.c
+++ wifi/wifi.c
@@ -318,11 +318,12 @@ int wifi_load_driver()
     char module_arg2[256];
 
 #ifdef SAMSUNG_WIFI
-#ifdef WIFI_DRIVER_MODULE_AP_ARG
+    char* type = get_samsung_wifi_type();
+
     if (wifi_mode == 1) {
-        snprintf(module_arg2, sizeof(module_arg2), DRIVER_MODULE_AP_ARG);
+        snprintf(module_arg2, sizeof(module_arg2), "%s%s", DRIVER_MODULE_AP_ARG, type == NULL ? "" : type);
     } else {
-        snprintf(module_arg2, sizeof(module_arg2), DRIVER_MODULE_ARG);
+        snprintf(module_arg2, sizeof(module_arg2), "%s%s", DRIVER_MODULE_ARG, type == NULL ? "" : type);
     }
 #else
     char* type = get_samsung_wifi_type();
